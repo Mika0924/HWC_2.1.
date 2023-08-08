b8e55c7d440eb9cedb467c316eea70f985667bfd
mika <mika.mnurmasheva.mika@mail.ru>
2023-08-08 21:56:14 +0600

    add Task_002 End

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+double a,b,c;
+Console.Write("число а ");
+a = Double.Parse(Console.ReadLine());
+Console.Write("число b ");
+b = Double.Parse(Console.ReadLine());
+Console.Write("число c ");
+c = Double.Parse(Console.ReadLine());
+    if (a>b)
+        if(a>c)
+        Console.WriteLine($"{a} больше");
+    if (c>b)
+        if(c>a)
+        Console.WriteLine($"{c} больше");
+    if (b>a)
+        if(b>c)
+        Console.WriteLine($"{b} больше");
